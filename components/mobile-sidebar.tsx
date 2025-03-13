5112226624495c6b5ecc65ddd306b1e14b605783
ethanenkhtur <98515458+ethanenkhtur@users.noreply.github.com>
2025-03-12 19:54:57 -0500

    added sheet component from shadcnui to accomplish a mobile sidebar that slides out from the left side with a button

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+    Sheet,
+    SheetContent,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
+
+export default function MobileSidebar() {
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Button
+                    variant={"ghost"}
+                    size={"icon"}
+                    className="md:hidden cursor-pointer"
+                >
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0">
+                <Sidebar />
+            </SheetContent>
+        </Sheet>
+    );
+}
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -67,6 +67,7 @@ export default function Sidebar() {
     return (
         <div className="py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
+                {/* Logo */}
                 <Link
                     href={"/dashboard"}
                     className="flex items-center pl-3 mb-14"
@@ -88,7 +89,7 @@ export default function Sidebar() {
                         <Link
                             href={route.href}
                             key={route.href}
-                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/20 rounded-lg transition"
+                            className="text-md group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/20 rounded-lg transition"
                         >
                             <div className="flex items-center flex-1">
                                 <route.icon
