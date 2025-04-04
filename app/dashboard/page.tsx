aff3a346336a85af058e6804e68902053fc79b79
Ethan Enkhtur <98515458+ethanenkhtur@users.noreply.github.com>
2025-04-03 20:28:21 -0500

    updated and simplified app routing in source code directories

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { ArrowRight, ImageIcon, MessageSquare } from "lucide-react";
+import Link from "next/link";
+
+export const tools = [
+	{
+		title: "Conversation",
+		icon: MessageSquare,
+		href: "/dashboard/conversation",
+		color: "text-violet-500",
+		bgColor: "bg-violet-500/10",
+	},
+	{
+		title: "Image Generation",
+		icon: ImageIcon,
+		href: "/dashboard/image",
+		color: "text-pink-700",
+		bgColor: "bg-pink-700/10",
+	},
+];
+
+export default function Dashboard() {
+	return (
+		<>
+			<header className="mb-6">
+				<h1 className="text-center text-3xl md:text-4xl lg:text-5xl font-extrabold mb-2">
+					Explore the power of AI
+				</h1>
+				<p className="text-center text-sm md:text-md lg:text-lg font-light ">
+					Chat with the smartest AI - Experience the power of AI
+				</p>
+			</header>
+			<main className="space-y-4">
+				{tools.map((tool) => (
+					<div className="px-10" key={tool.href}>
+						<Link
+							href={tool.href}
+							className="flex flex-col max-w-lg md:max-w-xl mx-auto"
+						>
+							<Card className="p-5 flex flex-row items-center justify-between transition hover:shadow-md">
+								<div className="flex gap-x-4 items-center">
+									<div
+										className={cn(
+											"w-fit p-2 rounded-md",
+											tool.bgColor
+										)}
+									>
+										<tool.icon
+											className={cn(
+												tool.color,
+												"h-6 w-6 md:h-8 md:w-8"
+											)}
+										/>
+									</div>
+									<p className="font-semibold text-md md:text-lg">
+										{tool.title}
+									</p>
+								</div>
+								<ArrowRight />
+							</Card>
+						</Link>
+					</div>
+				))}
+			</main>
+		</>
+	);
+}
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,101 +1,101 @@
 "use client";
 
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import {
-    ImageIcon,
-    LayoutDashboard,
-    MessageSquare,
-    Settings,
+	ImageIcon,
+	LayoutDashboard,
+	MessageSquare,
+	Settings,
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const items = [
-    {
-        title: "Dashboard",
-        icon: LayoutDashboard,
-        route: "/dashboard",
-        color: "text-sky-500",
-    },
-    {
-        title: "Conversation",
-        icon: MessageSquare,
-        route: "/conversation",
-        color: "text-violet-500",
-    },
-    {
-        title: "Image Generation",
-        icon: ImageIcon,
-        route: "/image",
-        color: "text-pink-700",
-    },
-    {
-        title: "Settings",
-        icon: Settings,
-        route: "/settings",
-    },
+	{
+		title: "Dashboard",
+		icon: LayoutDashboard,
+		route: "/dashboard",
+		color: "text-sky-500",
+	},
+	{
+		title: "Conversation",
+		icon: MessageSquare,
+		route: "/dashboard/conversation",
+		color: "text-violet-500",
+	},
+	{
+		title: "Image Generation",
+		icon: ImageIcon,
+		route: "/dashboard/image",
+		color: "text-pink-700",
+	},
+	{
+		title: "Settings",
+		icon: Settings,
+		route: "/settings",
+	},
 ];
 
 export function AppSidebar() {
-    const pathname = usePathname();
+	const pathname = usePathname();
 
-    return (
-        <Sidebar variant="inset">
-            <SidebarHeader>
-                <SidebarMenu>
-                    <SidebarMenuItem>
-                        <Link
-                            href={"/dashboard"}
-                            className="flex items-center ml-3 px-2 py-4"
-                        >
-                            <Image
-                                src={"/logo.png"}
-                                alt="Logo"
-                                height={35}
-                                width={35}
-                                className="mr-4"
-                            />
-                            <h1 className="text-2xl font-bold">Alldo</h1>
-                        </Link>
-                    </SidebarMenuItem>
-                </SidebarMenu>
-            </SidebarHeader>
-            <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            {items.map((item) => (
-                                <SidebarMenuItem
-                                    key={item.title}
-                                    className="ml-2"
-                                >
-                                    <SidebarMenuButton
-                                        asChild
-                                        isActive={pathname === item.route}
-                                    >
-                                        <Link href={item.route}>
-                                            <item.icon className={item.color} />
-                                            <span>{item.title}</span>
-                                        </Link>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            </SidebarContent>
-            <SidebarFooter />
-        </Sidebar>
-    );
+	return (
+		<Sidebar variant="inset">
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<Link
+							href={"/dashboard"}
+							className="flex items-center ml-3 px-2 py-4"
+						>
+							<Image
+								src={"/logo.png"}
+								alt="Logo"
+								height={35}
+								width={35}
+								className="mr-4"
+							/>
+							<h1 className="text-2xl font-bold">Alldo</h1>
+						</Link>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{items.map((item) => (
+								<SidebarMenuItem
+									key={item.title}
+									className="ml-2"
+								>
+									<SidebarMenuButton
+										asChild
+										isActive={pathname === item.route}
+									>
+										<Link href={item.route}>
+											<item.icon className={item.color} />
+											<span>{item.title}</span>
+										</Link>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+			<SidebarFooter />
+		</Sidebar>
+	);
 }
